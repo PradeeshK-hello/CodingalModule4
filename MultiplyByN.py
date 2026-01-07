ce9740ee696f71ca986edf68d4ec376d865d71fa
PradeeshK-hello <sp.pradeeshkumar@gmail.com>
2026-01-07 20:46:29 +0800

    Add files via upload

---
+++ MultiplyByN.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter your first number:"))
+num2 = int(input("Enter your second number:"))
+
+def multiply(n):
+    iteration=0
+    for i in range(0,n+1):
+        print("'n' Multiplication",num1*num2)
+        iteration+=1
+        
+def multiply():
+    iteration2=0
+    print("'1' Multiplication",num1*num2)
+    iteration2+=1
